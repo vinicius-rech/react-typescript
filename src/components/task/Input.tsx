eff3822e7b350c5249da0f1a0d5e34e74675249f
Vinicius Rech <contato@vinicius-rech.dev>
2022-09-17 14:06:47 -0300

    wip: implementes task input validation.

--- src/components/task/Input.tsx
+++ src/components/task/Input.tsx
@@ -2,10 +2,28 @@ import {TextField} from "@mui/material";
 import React from "react";
 
 export const Input = () => {
+  const [taskDescription, setTaskDescription] = React.useState('')
 
-  const onKeyPressed = (keyPressed: React.KeyboardEvent<HTMLDivElement>) => {
-    keyPressed.code === 'Enter' && console.log('ok')
+  const isEmptyField = (text: string): boolean => text === '' ? true : false
+
+  const dispatchAlert = () => {
+    console.log('campo vazio')
   }
+
+  const validateEmptyField = (text: string) =>
+    isEmptyField(text) ? dispatchAlert() : setTaskDescription(text)
+
+
+  const onKeyPressed = (_onKeyDown: React.KeyboardEvent<HTMLDivElement>) => {
+    const target = _onKeyDown.target as HTMLInputElement
+    if (_onKeyDown.code == 'Enter') {
+      validateEmptyField(target.value)
+    }
+  }
+
+  React.useEffect(() => {
+    console.log(taskDescription)
+  }, [taskDescription])
 
   return (
     <TextField fullWidth={true}
