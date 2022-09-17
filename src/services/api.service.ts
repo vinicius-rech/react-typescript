f7b7c42299d4835fa1c0dd92efb996d2c6184d25
Vinicius Rech <contato@vinicius-rech.dev>
2022-09-17 15:55:12 -0300

    wip: solving deploy problem.

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://api-nest-production.up.railway.app"
+const baseURL = "https://api-nest-production.up.railway.app/"
 const headers = {
   'Content-Type': 'application/json;charset=utf-8',
   "Access-Control-Allow-Origin": "*",
